e470509f6360b8ede75b0fe8b97a0b56476d3a9d
Akeem92 <34960651+Akeem92@users.noreply.github.com>
2021-08-12 22:57:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-  $host = 'db';
-  $user = 'devuser';
-  $password = 'devpass';
-  $db= 'test_db';
-
-  $conn = new mysqli($host,$user,$password,$db);
-  if($conn->connect_error){
-          echo 'Connection failed' . $conn->connect_error;
-  }
+  echo 'Bienvenu dans docker';
   echo 'Successfully connected to MYSQL';
 ?>
